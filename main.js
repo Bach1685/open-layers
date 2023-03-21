4d676b73c54379f4b7cfd5264bbd2303629c6dd7
Bach1685 <alekseimatyuhin@gmail.com>
2023-03-21 23:14:33 +0300

    group of layers

--- main.js
+++ main.js
@@ -4,20 +4,40 @@ function init () {
     const map = new ol.Map({
         view: new ol.View({
             center: [-7711498.573963709, -805216.7185664922],
-            zoom: 7,
-            maxZoom: 10,
-            mixZoom: 4,
-            rotation: 0.5
+            zoom: 2,
+            minZoom: 4,
         }),
-        layers:[
-            new ol.layer.Tile({
-                source: new ol.source.OSM()
-            })
-        ],
          target: 'js-map'
     })
 
-    map.on('click', function(e){
-        console.log(e.coordinate)
+    const openStreetMapStandard = new ol.layer.Tile({
+        source: new ol.source.OSM(),
+        visible: true,
+        title: 'OSMStandard'
     })
+
+   const openStreetMapHumanitarian = new ol.layer.Tile({
+    source: new ol.source.OSM({
+        url: 'https://{a-c}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+    }),
+    visible: false,
+    title: 'OSMHumanitarian'
+   })
+
+   const stamenTerrain = new ol.layer.Tile({
+    source: new ol.source.XYZ({
+        url: 'https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}.png',
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+    }),
+    visible: false,
+    title: 'StamenTerrain'
+   })
+
+   const baseLayerGroup = new ol.layer.Group({
+    layers: [
+        openStreetMapStandard, openStreetMapHumanitarian
+    ]
+   })
+
+   map.addLayer(baseLayerGroup)
 }
